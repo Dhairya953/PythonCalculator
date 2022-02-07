bc344bd3745efd46f84289ebca25dd12d271ab33
dhair <dhair@ILOVEASS>
2022-02-06 22:16:07 -0500

    Add project files.

---
+++ NewTets/NewTets.py
@@ -0,0 +1,24 @@
+print("Welcome to my calculator")
+
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+
+def add():
+    add=num1 + num2
+    print(add)
+def sub():
+    sub=num1 - num2
+    print(sub)
+def mul():
+    mul=num1 * num2
+    print(mul)
+def div():
+    div=num1 / num2
+    print(div)
+
+add()
+sub()
+mul()
+div()
+
+print("Thank you for using my calculator")
